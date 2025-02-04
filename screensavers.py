393d43b6dd3d08509a47f9234e5b0e0af3763c02
georg <gyazvinskiy@yandex.ru>
2025-02-04 17:14:30 +0300

    1.0.0

--- screensavers.py
+++ screensavers.py
@@ -8,13 +8,13 @@ class Screensaver:
         self.screen = pg.display.set_mode(size)
         self.running_gui_manager = running_gui_manager
         self.manager = pygame_gui.UIManager(size, 'resources/theme.json')
-        self.start_btn = pygame_gui.elements.UIButton(relative_rect=pg.Rect((470, 300, 500, 70)),
+        self.start_btn = pygame_gui.elements.UIButton(relative_rect=pg.Rect((width / 2 - 250, 300, 500, 70)),
                                                       text='Start',
                                                       manager=self.manager)
-        self.settings_btn = pygame_gui.elements.UIButton(relative_rect=pg.Rect((470, 400, 500, 70), ),
+        self.settings_btn = pygame_gui.elements.UIButton(relative_rect=pg.Rect((width / 2 - 250, 400, 500, 70), ),
                                                          text='Settings',
                                                          manager=self.manager)
-        self.exit_btn = pygame_gui.elements.UIButton(relative_rect=pg.Rect((470, 500, 500, 70), ),
+        self.exit_btn = pygame_gui.elements.UIButton(relative_rect=pg.Rect((width / 2 - 250, 500, 500, 70), ),
                                                      text='Exit',
                                                      manager=self.manager)
 
